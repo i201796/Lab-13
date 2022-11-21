244eabd1761b1936df886b67376e7f8432e08afe
Moiz Asghar <95476009+i201796@users.noreply.github.com>
2022-11-21 13:53:20 +0500

    Lab 13

---
+++ server.test.js
@@ -0,0 +1,18 @@
+const request = require('jest');
+const app = require('./routes/User');
+const mongoose = require('mongoose');
+
+const request = require('jest');
+const app = require('./routes/User');
+const mongoose = require('mongoose');
+
+
+test.todo('It should response the GET method'), () => {
+        const response = request(app).post("http://localhost:3001/users/add").json({
+            Name: "Test",
+            Email: "Test",
+            Age: 20,
+            Contact: 1234567890
+    })
+    expect(response.statusCode).toBe(200);
+};
